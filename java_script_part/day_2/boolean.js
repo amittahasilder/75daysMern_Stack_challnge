cba765ed9a53691c3446074cdaaf86c552a76433
amittahasilder <amittahasilder@gmail.com>
2026-03-28 07:41:54 -0700

    update file

---
+++ java_script_part/day_2/boolean.js
@@ -0,0 +1,5 @@
+let b1 = true;
+console.log(b1);  
+
+let b2 = false;
+console.log(b2);
---
+++ java_script_part/day_2/string.js
@@ -0,0 +1,8 @@
+let s1 = "Hello There";
+console.log(s1); 
+
+let s2 = 'Single quotes work fine';
+console.log(s2); 
+
+let s3 = `can embed ${s1}`;
+console.log(s3);
